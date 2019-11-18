0aa1288e5b15f232149669f7c41deb3b9605f2c6
topdeveloper1001 <topdotnetdeveloper1001@gmail.com>
2019-11-18 21:15:54 +0300

    implemented extracting text from pdf

---
+++ ExtractPDF/ExtractPDF/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Spire.Pdf;
+
+
+namespace ExtractPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Create a pdf document.
+            
+            PdfDocument doc = new PdfDocument();
+            
+            doc.LoadFromFile("Spec Sample Pages.pdf");
+            
+            StringBuilder buffer = new StringBuilder();
+            
+            
+            foreach (PdfPageBase page in doc.Pages)
+            {
+            
+                buffer.Append(page.ExtractText());
+            
+               
+                
+            }
+            
+            
+            doc.Close();
+            
+            
+
+        }
+    }
+}
